0e16d57f64a44159f0be098b006f6337aa11338e
JAVIERFERNANDEZVEGA <jfvtec@gmail.com>
2017-01-20 19:58:00 +0100

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,14 @@
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        myFunction(this);
+    }
+};
+xhttp.open("GET", "preguntas.xml", true);
+xhttp.send();
+
+function myFunction(xml) {
+    var xmlDoc = xml.responseXML;
+    document.getElementById("demo").innerHTML =
+    xmlDoc.getElementsByTagName("pregunta")[0].childNodes[0].nodeValue;
+}
